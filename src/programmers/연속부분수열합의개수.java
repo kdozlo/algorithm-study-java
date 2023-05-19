1dd7ef4a510eabc7504c5a54424f8d46ed5c6bd8
kdozlo <eodud5730@naver.com>
2023-05-20 00:05:47 +0900

    프로그래머스 - 연속 부분 수열 합의 개수 dp 사용

--- src/programmers/연속부분수열합의개수.java
+++ src/programmers/연속부분수열합의개수.java
@@ -1,5 +1,6 @@
 package programmers;
 
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -10,6 +11,29 @@ import java.util.Set;
  */
 public class 연속부분수열합의개수 {
 
+    public static void main(String[] args) {
+        int[] elements = {7,9,1,1,4};
+
+        Set<Integer> set = new HashSet<>();
+        int[][] dp = new int[elements.length][elements.length];
+
+        for(int i = 0; i < elements.length; i++) {
+            Arrays.fill(dp[i], 0);
+            dp[0][i] = elements[i];
+            set.add(dp[0][i]);
+        }
+
+        for(int i = 1; i < elements.length; i++) {
+            for(int j = 0; j < elements.length; j++) {
+                dp[i][j] +=  dp[i - 1][j] + elements[(j + i) % elements.length];
+                set.add(dp[i][j]);
+            }
+        }
+
+        System.out.println(set.size());
+    }
+
+   /* //삼중 포문
     public static void main(String[] args) {
         int[] elements = {7,9,1,1,4};
 
@@ -26,5 +50,5 @@ public class 연속부분수열합의개수 {
         }
 
         System.out.println(set.size());
-    }
+    }*/
 }
