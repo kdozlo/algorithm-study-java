8faeee3a492d15fa2d3b19a68a396e82c870d317
kdozlo <eodud5730@naver.com>
2023-05-19 14:30:58 +0900

    프로그래머스 - 괄호 회전하기

--- src/boj/Boj1753.java
+++ src/boj/Boj1753.java
@@ -11,7 +11,6 @@ import java.util.*;
  * 골드 4
  * https://www.acmicpc.net/problem/1753
  */
-
 class Node implements Comparable<Node>{
     int end;
     int w;
---
+++ src/programmers/괄호회전하기.java
@@ -0,0 +1,50 @@
+package programmers;
+
+import java.util.Stack;
+
+/**
+ * 프로그래머스 - 괄호 회전하기
+ * Lv 2
+ * https://school.programmers.co.kr/learn/courses/30/lessons/76502
+ */
+public class 괄호회전하기 {
+
+    public static void main(String[] arsg) {
+        String s = "[](){}";
+
+        int answer = 0;
+
+        String newS = s;
+        for(int i = 0; i < s.length(); i++){
+            if(checkBracket(newS)) answer++;
+            newS = newS.substring(1, s.length()) + newS.charAt(0);
+        }
+
+        System.out.println(answer);
+    }
+
+    public static boolean checkBracket (String s) {
+        Stack<Character> stack = new Stack<>();
+
+        if(!s.contains("[") || !s.contains("(") || !s.contains("{"))
+            return false;
+
+        for(Character c : s.toCharArray()) {
+            if (c == '[' || c == '(' || c =='{') {
+                stack.push(c);
+            } else if (c == ']' && !stack.isEmpty() && stack.peek() == '[') {
+                stack.pop();
+            } else if (c == ')' && !stack.isEmpty() && stack.peek() == '(') {
+                stack.pop();
+            } else if (c == '}' && !stack.isEmpty() && stack.peek() == '{') {
+                stack.pop();
+            }
+
+        }
+
+        if(stack.isEmpty())
+            return true;
+        else
+            return false;
+    }
+}
