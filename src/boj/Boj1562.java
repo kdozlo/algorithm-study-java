31b0c69a6552ca65c5f15ea095c17ee935b8f00c
kdozlo <eodud5730@naver.com>
2023-05-24 13:08:28 +0900

    BOJ - 1562

---
+++ src/boj/Boj1562.java
@@ -0,0 +1,22 @@
+package boj;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+/**
+ * 백준 1562
+ * 계단 수
+ * 골드 1
+ * https://www.acmicpc.net/problem/1562
+ * 못품 너무 어렵
+ */
+public class Boj1562 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+    }
+}
