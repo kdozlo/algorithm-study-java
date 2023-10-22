033a30b5d1a86324c28c475caeacf765ff264ff5
kdozlo <eodud5730@naver.com>
2023-10-22 16:46:21 +0900

    백준 : 사과나무 - 20002

--- src/boj/b20002.java
+++ src/boj/b20002.java
@@ -13,21 +13,38 @@ import java.util.StringTokenizer;
  */
 public class b20002 {
 
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static int n;
+    static int[][] apple;
+    static int[][] prefix;
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int n = Integer.parseInt(st.nextToken()); //과수원 크기
-
-        int[][] trees = new int[n][n];
-            for (int i = 0; i < n; i++) {
-                st = new StringTokenizer(br.readLine());
-                for (int j = 0; j < n; j++) {
-                    trees[i][j] = Integer.parseInt(st.nextToken());
-                }
+        n = Integer.parseInt(br.readLine());
+        apple = new int[n+1][n+1];
+        prefix = new int[n+1][n+1];
+        for (int i = 1; i <= n; i++) {
+            StringTokenizer str = new StringTokenizer(br.readLine());
+            for (int j = 1; j <= n; j++) {
+                apple[i][j] = Integer.parseInt(str.nextToken());
+                prefix[i][j]=prefix[i-1][j]+prefix[i][j-1]-prefix[i-1][j-1]+apple[i][j];
             }
-
-
-
+        }
+        long max = -1001;
+        for (int i = 1; i <= n; i++) {
+            long tmp = harvest(i);
+            max=Math.max(max, tmp);
+        }
+        System.out.println(max);
+    }
+    public static long harvest(int k){
+        long max = -1001;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                if(i+k-1>n||j+k-1>n) continue;
+                int tmp = prefix[i+k-1][j+k-1]-prefix[i-1][j+k-1]-prefix[i+k-1][j-1]+prefix[i-1][j-1];
+                max = Math.max(tmp, max);
+            }
+        }
+        return max;
     }
 }
