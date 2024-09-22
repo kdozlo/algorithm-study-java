60a631b5b4593f6b9f33600e2747a87f025111bb
kdozlo <eodud5730@naver.com>
2024-09-22 20:54:25 +0900

    BOJ 2212: 센서
    - https://kdozlo.tistory.com/52

--- src/boj/Boj2212.java
+++ src/boj/Boj2212.java
@@ -1,10 +1,7 @@
 package boj;
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+
+import java.io.*;
+import java.util.*;
 
 /**
  * 백준 2212
@@ -13,29 +10,36 @@ import java.util.StringTokenizer;
  * https://www.acmicpc.net/problem/2212
  */
 public class Boj2212 {
+
     public static void main (String[] args) throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st;
 
         int N = Integer.parseInt(br.readLine());
         int K = Integer.parseInt(br.readLine());
 
-        if (K >= N) {
-            bw.write("0\n");
-            bw.close();
-            br.close();
-            return;
+        if(N <= K)
+            System.out.println(0);
+        else {
+            int[] censor = new int[N];
+            st = new StringTokenizer(br.readLine());
+            for(int i = 0; i < N; i++)
+                censor[i] = Integer.parseInt(st.nextToken());
+
+            Arrays.sort(censor);
+            int[] dif = new int[N - 1];
+
+            for(int i = 0; i < N - 1; i++)
+                dif[i] = censor[i + 1] - censor[i];
+
+            Arrays.sort(dif);
+
+            int answer = 0;
+            for (int i = 0; i < N - K; i++) {
+                answer += dif[i];
+            }
+
+            System.out.println(answer);
         }
-
-        int[] censor = new int[N];
-        st = new StringTokenizer(br.readLine());
-        for(int i = 0; i < N; i++) {
-            int temp = Integer.parseInt(st.nextToken());
-            censor[i] = temp;
-        }
-        Arrays.sort(censor);
-
-
     }
 }
